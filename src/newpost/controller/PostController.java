493298a33942842baa5a7fe9ca84e7a6e1b853c8
VladyslavTaran <ko12@list.ru>
2016-07-24 14:44:15 +0300

    Add show post office controller, related post office interface and additional resources for geolocation.

---
+++ src/newpost/controller/PostController.java
@@ -0,0 +1,43 @@
+package newpost.controller;
+
+import newpost.db.AppDataContainer;
+import newpost.utils.geolocation.MapFramefork.ShowMapMarkerFrame;
+import newpost.utils.geolocation.controller.GoogleMapsAPI;
+import newpost.utils.geolocation.controller.GoogleMapsAPIImpl;
+import newpost.utils.geolocation.controller.Location;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Vladislav on 24.07.2016.
+ */
+public class PostController implements IPostController {
+
+    private AppDataContainer appDataContainer;
+
+    public PostController(AppDataContainer appDataContainer) {
+        this.appDataContainer = appDataContainer;
+    }
+
+    @Override
+    public void showOfficesOnMap() {
+        //todo create class post office and change ticket and db
+        GoogleMapsAPI googleMapsAPI = new GoogleMapsAPIImpl();
+        Location myLocation  = googleMapsAPI.findLocation("Україна", "Київ", "Старокиєвська", "10");
+
+        List<Location> locations = new ArrayList();
+        locations.add(googleMapsAPI.findLocation("Україна", "Київ", "Бульва Лесі Українки", "5"));
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new ShowMapMarkerFrame(myLocation, locations);
+            }
+        });
+
+
+    }
+}
