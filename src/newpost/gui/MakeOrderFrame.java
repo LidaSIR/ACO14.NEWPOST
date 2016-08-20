6f7aadc1d332254da27937fc58de91a8739a1968
alexandr38 <aleksandragryzkov@gmail.com>
2016-08-20 10:27:22 +0300

    Merge branch 'master' of https://github.com/foreveRush/ACO14.NEWPOST
    # Conflicts:
    #	src/newpost/controller/ClientController.java

--- src/newpost/gui/MakeOrderFrame.java
+++ src/newpost/gui/MakeOrderFrame.java
@@ -1,5 +1,10 @@
 package newpost.gui;
 
+import newpost.controller.ClientController;
+import newpost.db.AppDataContainer;
+import newpost.validator.ValidationClientControllerProxy;
+import newpost.validator.Validator;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -8,22 +13,43 @@ import java.awt.*;
  */
 public class MakeOrderFrame extends JFrame {
 
-    public MakeOrderFrame() {
-        setTitle("make order");
-        setSize(600, 600);
+    private static final int DEFAULT_WIDTH = 800;
+    private static final int DEFAULT_HEIGHT = 600;
+    private static final String TITLE = "make order";
+
+    private AppDataContainer appDataContainer;
+    private ValidationClientControllerProxy validationClientControllerProxy;
+    private ClientController clientController;
+
+    public MakeOrderFrame(AppDataContainer appDataContainer){
+        this.appDataContainer = appDataContainer;
+        this.clientController = new ClientController(appDataContainer);
+        this.validationClientControllerProxy = new ValidationClientControllerProxy(clientController, new Validator());
+    }
+
+    public static JFrame createFirstFrame() {
+        JFrame orderFrame = new JFrame();
+        orderFrame.setTitle(TITLE);
+        orderFrame.setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
         //CENTER
-        Dimension sSize = Toolkit.getDefaultToolkit().getScreenSize(), fSize = getSize();
+        Dimension sSize = Toolkit.getDefaultToolkit().getScreenSize(), fSize = orderFrame.getSize();
 
         if (fSize.height > sSize.height) {fSize.height = sSize.height;}
         if (fSize.width  > sSize.width)  {fSize.width = sSize.width;}
-        setLocation ((sSize.width - fSize.width)/2,
+        orderFrame.setLocation ((sSize.width - fSize.width)/2,
                 (sSize.height - fSize.height)/2);
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        setVisible(true);
+        
+
+        orderFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        orderFrame.setVisible(true);
+        return orderFrame;
     }
 
     public static void main(String[] args) {
-        MakeOrderFrame mof = new MakeOrderFrame();
+        createFirstFrame();
+        //AppDataContainer app = new AppDataContainer();
+        //ManagerView mv = new ManagerView(app);
+        //mv.showManagerView();
     }
 }
