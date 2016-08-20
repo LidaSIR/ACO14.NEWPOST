84eea5ecc61ad872e3f53c6534ddb8c44d873c8c
artpashchenko <pashchenko.artem.1988@gmail.com>
2016-08-20 15:09:34 +0300

    fx

--- src/newpost/view/console/MenuClient.java
+++ src/newpost/view/console/MenuClient.java
@@ -31,12 +31,16 @@ public class MenuClient extends Menu {
     }
 
     protected void showMenuClient() {
-        System.out.println("1. Add Ticket");
+        String[] clientMenu = {"1. Add Ticket","2. Show info ","3. Cancel order","4. Take product","5. Show all post offices","0. Exit"};
+        for (int i = 0; i <clientMenu.length ; i++) {
+            System.out.println(clientMenu[i]);
+        }
+       /* System.out.println("1. Add Ticket");
         System.out.println("2. Show info ");
         System.out.println("3. Cancel order");
         System.out.println("4. Take product");
         System.out.println("5. Show all post offices");
-        System.out.println("0. Exit");
+        System.out.println("0. Exit");*/
 
     }
 
