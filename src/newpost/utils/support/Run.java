523d1cd436c288305589f17eb2263b51cf3fe0c9
VladyslavTaran <ko12@list.ru>
2016-08-20 18:46:33 +0300

    Support functionality has been added. Additional sen email method has been added to SMTP class.

---
+++ src/newpost/utils/support/Run.java
@@ -0,0 +1,10 @@
+package newpost.utils.support;
+
+/**
+ * Created by Vladislav on 20.08.2016.
+ */
+public class Run {
+    public static void main(String[] args) {
+        new SupportForm();
+    }
+}
