f87f93dd8f200a0cb02b2c892f80cbc2985cddd9
admin <fyrsenko@gmail.com>
2016-08-20 17:30:49 +0300

    add sending login and pass to mail after creating new user

--- src/newpost/utils/email/smtp/SMTP.java
+++ src/newpost/utils/email/smtp/SMTP.java
@@ -29,58 +29,33 @@ public class SMTP {
             "\n" +
             "Now your order in progress and your ticket number is:\n" +
             "{ticket}";
-
+    private final static String DEFAULT_MESSAGE_LOGIN_AND_PASSWORD = "Hello dear {name}!!!\n" +
+            "\n" +
+            "Your login is: " + "{login}" +
+            "Your password is: " + "{pass}" ;
 
     public static void sendMail(Client to, PostTicket postTicket, String attachmentPath) throws IOException {
 
         if (to.getMail() == null) return;
 
-        Properties props = System.getProperties();
-        Map<String, String> propertiesMap = getPropertiesFromFile();
-
-        propertiesMap.keySet().stream().forEach((key) -> {
-            props.put(key, propertiesMap.get(key));
-        });
-
-        Session session = Session.getDefaultInstance(props);
-        MimeMessage message = new MimeMessage(session);
 
         String mailText = DEFAULT_MESSAGE_TEXT.replace("{name}", to.getPassport().getFullname());
         mailText = mailText.replace("{ticket}", postTicket.getId());
 
 
-        try {
-            message.setFrom(new InternetAddress(propertiesMap.get(SMTP_LOGIN_KEY)));
-            InternetAddress toAddress = new InternetAddress(to.getMail());
+        send(to, mailText, attachmentPath);
+    }
 
-            message.addRecipient(Message.RecipientType.TO, toAddress);
-            message.setSubject(MAIL_SUBJECT);
+    public static void sendLoginAndPass(Client to) throws IOException {
 
-            BodyPart messageBodyPart = new MimeBodyPart();
-            messageBodyPart.setText(mailText);
-            Multipart multipart = new MimeMultipart();
-            multipart.addBodyPart(messageBodyPart);
+        if (to.getMail() == null) return;
 
-            // attachment
-            if(attachmentPath!=null) {
-                messageBodyPart = new MimeBodyPart();
-                String filename = attachmentPath;
-                DataSource source = new FileDataSource(filename);
-                messageBodyPart.setDataHandler(new DataHandler(source));
-                messageBodyPart.setFileName(filename);
-                multipart.addBodyPart(messageBodyPart);
-            }
-            message.setContent(multipart);
+        String mailText = DEFAULT_MESSAGE_LOGIN_AND_PASSWORD.replace("{name}", to.getPassport().getFullname());
+        mailText = mailText.replace("{login}", to.getLogin());
+        mailText = mailText.replace("{pass}", to.getPassword());
 
-            Transport transport = session.getTransport("smtp");
-            transport.connect(propertiesMap.get(SMTP_HOST_KEY), propertiesMap.get(SMTP_LOGIN_KEY), propertiesMap.get(SMTP_PASSWORD_KEY));
-            transport.sendMessage(message, message.getAllRecipients());
-            transport.close();
-        } catch (AddressException ae) {
-            ae.printStackTrace();
-        } catch (MessagingException me) {
-            me.printStackTrace();
-        }
+        send(to, mailText, null);
+
     }
 
     private static Map<String, String> getPropertiesFromFile() throws IOException {
@@ -107,4 +82,49 @@ public class SMTP {
 
         return propertiesMap;
     }
+
+    private static void send (Client client, String mailText, String attachmentPath) throws IOException {
+
+        Properties props = System.getProperties();
+        Map<String, String> propertiesMap = getPropertiesFromFile();
+
+        props.setProperty("mail.smtp.tls.trust", "smtp.gmail.com");
+        Session session = Session.getDefaultInstance(props);
+        MimeMessage message = new MimeMessage(session);
+        try {
+            message.setFrom(new InternetAddress(propertiesMap.get(SMTP_LOGIN_KEY)));
+            InternetAddress toAddress = new InternetAddress(client.getMail());
+
+            message.addRecipient(Message.RecipientType.TO, toAddress);
+            message.setSubject(MAIL_SUBJECT);
+
+            BodyPart messageBodyPart = new MimeBodyPart();
+            messageBodyPart.setText(mailText);
+            Multipart multipart = new MimeMultipart();
+            multipart.addBodyPart(messageBodyPart);
+
+            // attachment
+            if (attachmentPath != null) {
+                messageBodyPart = new MimeBodyPart();
+                String filename = attachmentPath;
+                DataSource source = new FileDataSource(filename);
+                messageBodyPart.setDataHandler(new DataHandler(source));
+                messageBodyPart.setFileName(filename);
+                multipart.addBodyPart(messageBodyPart);
+            }
+            message.setContent(multipart);
+
+            Transport transport = session.getTransport("smtp");
+            transport.connect(propertiesMap.get(SMTP_HOST_KEY), propertiesMap.get(SMTP_LOGIN_KEY), propertiesMap.get(SMTP_PASSWORD_KEY));
+            transport.sendMessage(message, message.getAllRecipients());
+            transport.close();
+        } catch (AddressException ae) {
+            ae.printStackTrace();
+        } catch (MessagingException me) {
+            me.printStackTrace();
+        }
+    }
+
+
+
 }
