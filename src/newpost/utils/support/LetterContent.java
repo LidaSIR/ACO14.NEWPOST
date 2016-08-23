851bb8f2e1993287c2f78b99f229656584af794c
VladyslavTaran <ko12@list.ru>
2016-08-24 00:21:15 +0300

    Improved reply message for support

--- src/newpost/utils/support/LetterContent.java
+++ src/newpost/utils/support/LetterContent.java
@@ -1,13 +1,14 @@
 package newpost.utils.support;
 
 import newpost.utils.email.model_letter.Letter;
-import newpost.utils.email.smtp.SMTP;
 
 import javax.swing.*;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.IOException;
 
 /**
  * Created by Vladislav on 20.08.2016.
@@ -15,7 +16,7 @@ import java.io.IOException;
 public class LetterContent extends JFrame {
     private Letter letter;
     private ISupportController controller;
-    JTextArea mailContent = new JTextArea();
+    JTextPane textPane = new JTextPane();
 
     public LetterContent(Letter letter, ISupportController controller){
         this.controller = controller;
@@ -28,11 +29,26 @@ public class LetterContent extends JFrame {
     }
 
     private void init(Letter letter){
-        mailContent.setText(letter.getMessage());
+        StyledDocument doc = textPane.getStyledDocument();
+
+        SimpleAttributeSet attributeSet = new SimpleAttributeSet();
+        StyleConstants.setForeground(attributeSet, Color.black);
+        StyleConstants.setBackground(attributeSet, Color.cyan);
+        StyleConstants.setFontSize(attributeSet, 12);
+        StyleConstants.setItalic(attributeSet, true);
+
+        try {
+            doc.insertString(0, letter.getMessage(), attributeSet);
+            doc.insertString(0, "\n\n\n\n\n", null);
+            textPane.setCaretPosition(0);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(this, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+        }
+
         JButton reply = new JButton("Reply");
         reply.addActionListener(new ReplyActionListener());
         JScrollPane scrollPane = new JScrollPane();
-        scrollPane.setViewportView(mailContent);
+        scrollPane.setViewportView(textPane);
         JPanel panel = new JPanel(new BorderLayout());
         panel.add(scrollPane, BorderLayout.CENTER);
         panel.add(reply, BorderLayout.SOUTH);
@@ -43,7 +59,7 @@ public class LetterContent extends JFrame {
     private class ReplyActionListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            controller.sendEmail(letter.getFromName(), mailContent.getText());
+            controller.sendEmail(letter.getFromName(), textPane.getText());
             dispose();
         }
     }
